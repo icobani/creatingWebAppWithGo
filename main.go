aa3ab678e887bb7459e089dcb204455cb5486adb
ibrahimcobani <ibrahim@imaconsult.com>
2016-12-26 21:08:30 +0300

    add public directory

--- main.go
+++ main.go
@@ -14,14 +14,36 @@ package main
 
 import (
 	"net/http"
+	"io/ioutil"
 )
-
 
 func index(w http.ResponseWriter, req *http.Request) {
 	w.Write([]byte("Babaley nittin."))
 }
 
 func main() {
-	http.HandleFunc("/pisi", index)
+	//http.HandleFunc("/pisi", index)
+
+	http.Handle("/", new(MyHandler))
+
 	http.ListenAndServe(":8000", nil)
+}
+
+type MyHandler struct {
+	http.Handler
+}
+
+func (this *MyHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	path := "./public/" + req.URL.Path
+	data, err := ioutil.ReadFile(string(path))
+
+	if err == nil {
+		var contentType string
+
+		w.Write(data)
+	} else {
+		w.WriteHeader(404)
+		w.Write([]byte("404 - " + http.StatusText(404)))
+	}
+
 }
